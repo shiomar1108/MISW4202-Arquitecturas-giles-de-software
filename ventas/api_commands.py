5a7755ddfd96bbf5ee32a41c16cfad66de972fdb
Oscar Tobar <o.tobar@uniandes.edu.co>
2023-02-22 19:41:53 -0500

    se adiciona validacion

--- ventas/api_commands.py
+++ ventas/api_commands.py
@@ -1,12 +1,46 @@
 from base import app, api, ma, q, Resource, Flask, request
 import socket
 from datetime import datetime
-
+import json
+import jsonschema
+from jsonschema import validate
 from sender import send_venta
 
 
 class VentaListResource(Resource):
 
+    def valida_json(jsonData):
+        try:
+            json.loads(jsonData)
+        except ValueError as err:
+            return False
+        return True
+    
+    def valida_estructura(jsonData):
+        
+        ventaSchema = {
+            "type": "object",
+            "properties": {
+                "cliente": {"type": "string"},
+                "clienteID": {"type": "number"},
+                "direccion": {"type": "string"},
+                "ciudad": {"type": "string"},
+                "vendedor": {"type": "string"},
+                "fechaPedido": {"type": "string"},
+                "fechaEntrega": {"type": "string"},
+                "metodoPago": {"type": "string"},
+                
+            },
+        }
+        try:
+            validate(instance=jsonData, schema=ventaSchema)
+        except jsonschema.exceptions.ValidationError as err:
+            return False
+        return True
+    
+    def valida_campos(self):
+        return False
+    
     def post(self):
         # Validación de los campos de entrada y enmascaramiento
         orden_validada = {
@@ -19,6 +53,10 @@ class VentaListResource(Resource):
             "metodoPago": request.json["metodoPago"],
             "productos": request.json["productos"]
         }
+        
+   
+
+ 
         # Enviamos la orden a la cola de Redis
         q.enqueue(send_venta, orden_validada)
         # Obenemos la ip del servidor que toma la petición
