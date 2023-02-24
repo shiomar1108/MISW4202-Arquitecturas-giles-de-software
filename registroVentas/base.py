6c46d49b694fde3ced02ae93202cfdc6a8f32e81
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-02-24 08:00:36 -0600

    See agrega proyecto base de redis

--- registroVentas/base.py
+++ registroVentas/base.py
@@ -1,21 +1,15 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_restful import Api, Resource
-import requests
-from redis import Redis
+import redis
+
+r = redis.Redis(
+    host='127.0.0.1',
+    port=6379,
+    decode_responses=True
+)
+
+cola = r.pubsub()
+
+cola.subscribe('ventas')
 
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/registroVentas.db'
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-api = Api(app)
-#q = Redis(host='localhost', port=6379, db=0)
-q = Redis(host='redis', port=6379, db=0)
-
-
-
-def process_venta(venta_id):
-    print('Se procesa venta')
+for message in cola.listen():
+    print(message['data'])
