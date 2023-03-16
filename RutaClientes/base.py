5140d291b12716405e73dfb217a78325fdf1bc26
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-03-15 22:37:59 -0600

    Se agrega componente Usuario con LogIn y validaciones de rol

--- RutaClientes/base.py
+++ RutaClientes/base.py
@@ -8,44 +8,47 @@ from flask_restful import Api
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
-
 class RutaClientes(Resource):
     @jwt_required()
     def get(self):
         headers = {'Authorization': request.headers['Authorization']}
         user = requests.get(f"http://localhost:5000/cpp/users/{request.json['user']}", headers=headers)
         hostIp = socket.gethostbyname(socket.gethostname())
-        if user.status_code==200 and request.json['user'] == 1:
-            response = {
-                "HTTPCode": 200,
-                "IP": hostIp,
-                "user": request.json['user'],
-                "RutaEntrega": {
-                    "Cliente1": "Tienda de Pedro",
-                    "Direccion1": "Santa Fe, Bogota",
-                    "Cliente2": "Tienda de Juan",
-                    "Direccion2": "Santa Teresa, Bogota",
-                    "Cliente3": "Tienda de Felipe",
-                    "Direccion3": "Kennedy, Bogota",
-                    "Cliente4": "Tienda de Pedro",
-                    "Direccion4": "Puente Aranda, Bogota",
-                },
-            }
+        if user.json()['rol'] == 'REP':
+            if user.status_code==200 and request.json['user'] == 1  :
+                response = {
+                    "HTTPCode": 200,
+                    "IP": hostIp,
+                    "user": request.json['user'],
+                    "RutaEntrega": {
+                        "Cliente1": "Tienda de Pedro",
+                        "Direccion1": "Santa Fe, Bogota",
+                        "Cliente2": "Tienda de Juan",
+                        "Direccion2": "Santa Teresa, Bogota",
+                        "Cliente3": "Tienda de Felipe",
+                        "Direccion3": "Kennedy, Bogota",
+                        "Cliente4": "Tienda de Pedro",
+                        "Direccion4": "Puente Aranda, Bogota",
+                    },
+                }
+            else:
+                response = {
+                    "HTTPCode": 409,
+                    "Mensaje": "No eres el usuario correcto",
+                    "IP": hostIp,
+                }
         else:
             response = {
-                "HTTPCode": 409,
-                "IP": hostIp,
-            }
-
+                    "HTTPCode": 409,
+                    "Mensaje": "No tienes el rol correcto",
+                    "IP": hostIp,
+                }
         return response
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config["JWT_SECRET_KEY"] = "secret-jwt"  # Change this!
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
+app.config["JWT_SECRET_KEY"] = "JwBGj2B4XFAKhYmn8Pgk0vH2w7UvgYfXAJ32e5rs8vI="
 jwt = JWTManager(app)
-
-
 app_context = app.app_context()
 app_context.push()
 api = Api(app)
--- usuarios/api.py
+++ usuarios/api.py
@@ -1,3 +1,5 @@
+import enum
+import hashlib
 from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -7,13 +9,13 @@ from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import fields, Schema
+from flask_jwt_extended import jwt_required, create_access_token
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbapp.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config["JWT_SECRET_KEY"] = "secret-jwt"  # Change this!
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
+app.config["JWT_SECRET_KEY"] = "JwBGj2B4XFAKhYmn8Pgk0vH2w7UvgYfXAJ32e5rs8vI="
 
 app_context = app.app_context()
 app_context.push()
@@ -23,18 +25,23 @@ cors = CORS(app)
 api = Api(app)
 jwt = JWTManager(app)
 
+class RolType(enum.Enum):
+    ADMINISTRADOR = 1
+    VENDEDOR = 2
+    REPARTIDOR = 3
+    CONTADOR = 4
+    ALMACENISTA = 5
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
-
-
+    password = db.Column(db.String(50))
+    rol = db.Column(db.String(3))
 
 class UserSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = User
         id = fields.String()
-
-
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
@@ -42,16 +49,18 @@ users_schema = UserSchema(many=True)
 db.init_app(app)
 db.create_all()
 
-
 class UserListResource(Resource):
     @jwt_required()
     def get(self):
         posts = User.query.all()
         return users_schema.dump(posts)
-    @jwt_required()
     def post(self):
+        contrasena_encriptada = hashlib.md5(
+                    request.json["password"].encode('utf-8')).hexdigest()
         new_user = User(
             username=request.json['username'],
+            password=contrasena_encriptada,
+            rol=request.json['rol'],
         )
         db.session.add(new_user)
         db.session.commit()
@@ -63,12 +72,19 @@ class UserResource(Resource):
     def get(self, user_id):
         user = User.query.get_or_404(user_id)
         return user_schema.dump(user)
-
-
+    
+class UserLogIn(Resource):
+    def post(self):
+        contrasena_encriptada = hashlib.md5(request.json["password"].encode('utf-8')).hexdigest()
+        usuario = User.query.filter(User.username == request.json["username"],
+                                       User.password == contrasena_encriptada).first()
+        db.session.commit()
+        token_de_acceso = create_access_token(identity=usuario.id)
+        return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": usuario.id, "rol": usuario.rol}
 
 api.add_resource(UserListResource, '/cpp/users')
 api.add_resource(UserResource, '/cpp/users/<int:user_id>')
-
+api.add_resource(UserLogIn, '/cpp/logIn')
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost',port=5000)
